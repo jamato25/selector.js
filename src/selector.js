50b923c01ef66918b4b3c2a19ecccbe3f715e7d3
James Amato <jamato2514@gmail.com>
2020-07-16 20:52:48 -0400

    First half

--- src/selector.js
+++ src/selector.js
@@ -1,1 +1,25 @@
-// YOUR CODE HERE
+// // YOUR CODE HERE
+class ${
+  constructor(str){
+    let obj;
+    let arr = [];
+    if(str[0]==="."){
+      str=str.substring(1);
+      obj = document.getElementsByClassName(str);
+      this.elements = [...obj];
+    }
+    else if(str[0]==="#"){
+      str=str.substring(1);
+      obj = document.getElementById(str);
+      arr.push(obj);
+      this.elements = arr;
+    }
+    else {
+      obj = document.getElementsByTagName(str);
+      this.elements = [...obj];
+    }
+
+  }
+
+
+}
